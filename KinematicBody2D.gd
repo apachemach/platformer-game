7c0e6818efc840c5129fccda0c70bcf126e4ff41
pemiliano393@gmail.com <pemiliano393@gmail.com>
2019-11-04 13:52:23 -0800

    added physics and gravity to player

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,10 +3,15 @@ extends KinematicBody2D
 var motion = Vector2() 
 
 func _physics_process(delta):
+	motion.y += 10 
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
 		
-	if Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_pressed('ui_left'):
 		motion.x = -100
+		
+		
+	else:
+		motion.x = 0
 	
 	move_and_slide(motion)
